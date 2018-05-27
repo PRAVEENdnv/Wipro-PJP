f591b19faac9fd581315d211f468f86cbe7c5674
Rohit Das <rohit.das950@gmail.com>
2018-05-27 23:19:31 +0530

    Completed Hands on for Java Fundamentals
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- java_fundamentals/Arrays/Prog9.java
+++ java_fundamentals/Arrays/Prog9.java
@@ -10,7 +10,26 @@ O/P:10
 */
 
 class Prog9 {
+	int linSrch(int arr[], int key) {
+		int index = -1;
+		for (int i = 0;i < arr.length;i++) {
+			if (key == arr[i]) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+
     public static void main(String[] args) {
-        
+        int arr[] = {1,6,4,7,9};
+        Prog9 obj = new Prog9();
+        int index1 =  obj.linSrch(arr,6), index2 = obj.linSrch(arr, 7);
+        if (index1 < index2) {
+        	for (int i = index1;i <= index2;i++) arr[i] = 0;
+        }
+    	int sum = 0;
+    	for (int i:arr) sum += i;
+    	System.out.println(sum);
     }
 }
