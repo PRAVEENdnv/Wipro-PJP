6987d42632032e3e505f8d9e91ee4073d923e337
Rohit Das <rohit.das950@gmail.com>
2018-06-15 14:21:32 +0530

    Added files for abstraction
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- Abstraction-Packages-Exception Handling/Exception Handling/Prog1.java
+++ Abstraction-Packages-Exception Handling/Exception Handling/Prog1.java
@@ -16,12 +16,13 @@ class Prog1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int a;
+        int a = 0;
         try {
             a = Integer.parseInt(sc.next());
         }
         catch (NumberFormatException e) {
             System.out.println("Entered input is not a valid format for an integer.");
+            return;
         }
         System.out.println("The square value is " + (a*a));
         System.out.println("The work has been done successfully");
